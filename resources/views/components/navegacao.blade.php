d5d2c4e9b494f75b07a955f2e887fc50371cb0bf
Leila Mondlane <leila.mondlane25@gmail.com>
2023-11-21 11:22:19 +0200

    up

---
+++ resources/views/components/navegacao.blade.php
@@ -0,0 +1,38 @@
+<ul class="nav flex-column">
+    <li class="nav-item">
+      <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="#">
+        <svg class="bi"><use xlink:href="#house-fill"/></svg>
+        Dashboard
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link d-flex align-items-center gap-2" href="#">
+        <svg class="bi"><use xlink:href="#file-earmark"/></svg>
+        Vendas
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link d-flex align-items-center gap-2" href="#">
+        <svg class="bi"><use xlink:href="#cart"/></svg>
+        Productos
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link d-flex align-items-center gap-2" href="#">
+        <svg class="bi"><use xlink:href="#people"/></svg>
+        Clientes
+      </a>
+    {{--</li>
+    <li class="nav-item">
+      <a class="nav-link d-flex align-items-center gap-2" href="#">
+        <svg class="bi"><use xlink:href="#graph-up"/></svg>
+        Reports
+      </a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link d-flex align-items-center gap-2" href="#">
+        <svg class="bi"><use xlink:href="#puzzle"/></svg>
+        Integrations
+      </a>
+    </li>--}}
+  </ul>
